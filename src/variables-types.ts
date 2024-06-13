3944841f8a9716688032a15339f951068a3b3df8
Angela Yepes <angelyep76@gmail.com>
2024-06-13 08:31:24 -0500

    tipado primitivo

---
+++ src/variables-types.ts
@@ -0,0 +1,40 @@
+let bookTitle: string = "TypeScript Basics";
+
+const libraryName: string = "My Library";
+
+let numberOfBooks: number = 13;
+
+let isLibraryOpen: boolean = true;
+
+
+let titulo: string =  "Culpa Mia"; 
+
+let autor: string = "Mercedes Ron"; 
+
+let genero: string = "Romance adolescente" ; 
+
+let idioma: string = "Español España"; 
+
+let precio: number = 56000 ; 
+
+let formato: string = "Tapa blanda"; 
+
+let isbn: string = "9788490438664"; 
+
+let descripcion: string = "Nicholas Leister ha sido creado para amargarme la vida. Alto, ojos azules, pelo negro como la noche... Suena genial ¿verdad? Pues no tanto cuando te enteras de que va a ser tu hermanastro y además representa todo de lo que has estado huyendo desde que tienes uso de razón.Peligro fue lo primero que me vino a la cabeza cuando lo conocí y descubrí que mantiene una doble vida oculta de su padre multimillonario. ¿Cómo termine cayendo en sus redes? Fácil: con esos ojos es capaz de poner tu mundo patas arriba."; 
+
+let estado: string = "Nuevo"; 
+
+let ubicacion: string = "Panamericana"; 
+
+let fecha_publicacion: string = "08/06/2017"; 
+
+let editorial: string = "MONTENA"; 
+
+let paginas: number = 448; 
+
+let dimensiones: string = " ancho: 15.2 cm x alto: 21.5 cm x grueso: 3.2 cm" ; 
+
+let peso: number = 544; 
+
+let disponible: boolean = true; 
